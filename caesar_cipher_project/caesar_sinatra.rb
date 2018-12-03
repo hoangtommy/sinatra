9b50c25a0230c5ec5140027b91f80b965dca5744
Tommy <t@tommyhoang.com>
2018-12-03 09:36:22 -0800

    complete cipher ui

--- caesar_cipher_project/caesar_sinatra.rb
+++ caesar_cipher_project/caesar_sinatra.rb
@@ -1,8 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-message = 'Caesar Cipher Encoder'
+def caesar_cipher(string, number)
+	return string.chars.map {|c|
+		(number%26).times{c.next!} if c.ord.between?(65, 90) || c.ord.between?(97, 122)
+		c[-1]
+	}.join("")
+end
 
 get '/' do
-  erb :index, :locals => { :message => message }
+  message = params['message'].to_s
+  code = params['code'].to_i
+  encoded_message = caesar_cipher(message, code)
+  erb :index, :locals => { :message => message, :code => code, :encoded_message => encoded_message }
 end
+
