16fdf6d072f166449ac1e364239927320af83280
Tommy <t@tommyhoang.com>
2018-12-05 18:04:08 -0800

    rearrange files and folder

---
+++ monstas/monstas.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+if development?
+  require 'sinatra/reloader'
+end
+
+enable :sessions
+
+# writing to an external file
+def store_name(filename, string)
+  File.open(filename, 'a+') do |file|
+  	file.puts(string)
+  end
+end
+
+def read_names
+  return [] unless File.exist?('names.txt')
+  File.read('names.txt').split('\n')
+end
+
+post '/index' do
+  @name = params[:name]
+  store_name('names.txt', @name)
+  session[:message] = "successfully logged #{@name}."
+  redirect "/index?name=#{@name}"
+end
+
+get '/index' do
+  @message = session.delete(:message)
+  @name = params[:name]
+  @names = read_names
+  erb :index
+end
